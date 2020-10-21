0a922eefa3426443e16c0bb55a02d7f4976a06ca
tarpaha <ppopchikovsky@gmail.com>
2020-10-21 23:31:26 +0300

    working get on root

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+const answers = require('./answers');
 
 const app = express();
+app.get('/', answers.get);
 
 const port = 8000;
 app.listen(port, () => {
-    console.log('We are live on ' + port);
+    console.log('Server started on: ' + port);
 });
