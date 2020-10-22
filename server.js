f06d25726654611d22905e66165de91baf901187
tarpaha <ppopchikovsky@gmail.com>
2020-10-22 17:26:35 +0300

    reverted clustering, too low advantage

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 'use strict';
 
+const express = require('express');
+const answers = require('./answers');
+
+const app = express();
+app.use(express.json());
+
+app.post('/', function(req, res) {
+    res.send({
+        answer: answers.get(req.body.text)
+    });
+});
+
 const port = 8000;
-
-const throng = require('throng');
-throng(start);
-
-function start() {
-    const express = require('express');
-    const answers = require('./answers');
-    
-    const app = express();
-    app.use(express.json());
-    
-    app.post('/', function(req, res) {
-        res.send({
-            answer: answers.get(req.body.text)
-        });
-    });
-    
-    app.listen(port, () => {
-        console.log('Server started on: ' + port);
-    });
-}
+app.listen(port, () => {
+    console.log('Server started on: ' + port);
+});
