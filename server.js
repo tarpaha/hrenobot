667015b6f8c545e30412ee3d485d20a117d1c408
tarpaha <ppopchikovsky@gmail.com>
2020-10-22 16:27:59 +0300

    throng for server clustering

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 'use strict';
 
-const express = require('express');
-const answers = require('./answers');
-
-const app = express();
-app.use(express.json());
-
-app.post('/', function(req, res) {
-    res.send({
-        answer: answers.get(req.body.text)
-    });
-});
-
 const port = 8000;
-app.listen(port, () => {
-    console.log('Server started on: ' + port);
-});
+
+const throng = require('throng');
+throng(start);
+
+function start() {
+    const express = require('express');
+    const answers = require('./answers');
+    
+    const app = express();
+    app.use(express.json());
+    
+    app.post('/', function(req, res) {
+        res.send({
+            answer: answers.get(req.body.text)
+        });
+    });
+    
+    app.listen(port, () => {
+        console.log('Server started on: ' + port);
+    });
+}
