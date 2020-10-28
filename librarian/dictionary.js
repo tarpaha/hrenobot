033c7f755deaba2d38c2681db17a7a63357a595e
tarpaha <ppopchikovsky@gmail.com>
2020-10-29 02:52:38 +0300

    ignoring case

--- librarian/dictionary.js
+++ librarian/dictionary.js
@@ -5,14 +5,15 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 var Dictionary;
 module.exports.getRecords = () => { return Dictionary; }
 module.exports.getReaction = (text) => {
-    const foundElement = Dictionary.find(el => el.text === text);
+    const textInLowerCase = text.toLowerCase();
+    const foundElement = Dictionary.find(el => el.text === textInLowerCase);
     return { reaction: foundElement ? foundElement.reaction : null };
 }
 module.exports.update = async () => {
     const spreadsheet = await loadSpreadsheet(process.env.SPREADSHEET_ID, process.env.API_KEY);
     const rows = await loadWorksheetRows(spreadsheet, 0);
     Dictionary = rows.map(row => {
-        return { text: row.text, reaction: row.reaction };
+        return { text: row.text.toLowerCase(), reaction: row.reaction };
     });
 }
 
